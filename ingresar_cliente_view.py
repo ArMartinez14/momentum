942cc6d438dcfccc715ab0a19b80cc0dfe0e4f39
Ariel Martinez <arielmartinez@MacBook-Air-de-Ariel.local>
2025-06-15 14:39:38 -0400

    cambios credencial en ingrresar cliente nuevo

--- ingresar_cliente_view.py
+++ ingresar_cliente_view.py
@@ -2,10 +2,12 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 import unicodedata
+import json
 
 # === INICIALIZAR FIREBASE SOLO UNA VEZ ===
 if not firebase_admin._apps:
-    cred = credentials.Certificate("aplicacion-asesorias-firebase-adminsdk-fbsvc-71e1560593.json")
+    cred_dict = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
