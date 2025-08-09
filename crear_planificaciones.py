205b230e6e1edf05a52f43d8c2600fe8f729d69b
ArMartinez14 <armartinezsaez@gmail.com>
2025-08-09 11:44:43 -0400

    Update crear_planificaciones.py

--- crear_planificaciones.py
+++ crear_planificaciones.py
@@ -11,7 +11,7 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
-from notificaciones_service import diff_rutina, crear_notificaciones
+
 import unicodedata
 from datetime import date, timedelta
 
